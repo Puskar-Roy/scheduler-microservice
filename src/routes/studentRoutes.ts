f6d060812165a29f6a2d87e1c08723c9b08158eb
Puskar-Roy <puskarroy600@gmail.com>
2025-03-15 20:25:16 +0530

    fiz : routes

--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -3,26 +3,26 @@ import StudentGroup from "../models/StudentGroup";
 
 const router = express.Router();
 
-// Get all student groups
+
 router.get("/", async (req, res) => {
   const groups = await StudentGroup.find();
   res.json(groups);
 });
 
-// Create a new student group
+
 router.post("/", async (req, res) => {
   const group = new StudentGroup(req.body);
   await group.save();
   res.status(201).json(group);
 });
 
-// Get a single student group by ID
+
 router.get("/:id", async (req, res) => {
   const group = await StudentGroup.findById(req.params.id);
   res.json(group);
 });
 
-// Update a student group
+
 router.put("/:id", async (req, res) => {
   const group = await StudentGroup.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -30,7 +30,7 @@ router.put("/:id", async (req, res) => {
   res.json(group);
 });
 
-// Delete a student group
+
 router.delete("/:id", async (req, res) => {
   await StudentGroup.findByIdAndDelete(req.params.id);
   res.status(204).send();
--- src/routes/subjectRoutes.ts
+++ src/routes/subjectRoutes.ts
@@ -3,26 +3,26 @@ import Subject from "../models/Subject";
 
 const router = express.Router();
 
-// Get all subjects
+
 router.get("/", async (req, res) => {
   const subjects = await Subject.find();
   res.json(subjects);
 });
 
-// Create a new subject
+
 router.post("/", async (req, res) => {
   const subject = new Subject(req.body);
   await subject.save();
   res.status(201).json(subject);
 });
 
-// Get a single subject by ID
+
 router.get("/:id", async (req, res) => {
   const subject = await Subject.findById(req.params.id);
   res.json(subject);
 });
 
-// Update a subject
+
 router.put("/:id", async (req, res) => {
   const subject = await Subject.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -30,7 +30,7 @@ router.put("/:id", async (req, res) => {
   res.json(subject);
 });
 
-// Delete a subject
+
 router.delete("/:id", async (req, res) => {
   await Subject.findByIdAndDelete(req.params.id);
   res.status(204).send();
